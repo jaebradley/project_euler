865678ec1b1d62faf13f8e1b73cfbad77314fd65
jaebradley <jae.b.bradley@gmail.com>
2015-01-09 23:08:44 -0500

    project euler
    e32 made some basic changes

--- pe32.py
+++ pe32.py
@@ -1,5 +1,11 @@
 __author__ = 'jaebradley'
 
+'''
+Want to find all numbers for m * n = z where concat(m, n, z) uses all digits from 1-9 exactly once
+Only 2 ways to get this: either m is a 1 digit number and n is a 4 digit number OR m is a 2 digit number and n is a 3 digit number
+So iterate through all possible m and n and check if concat(m, n, z) is pandigital
+'''
+
 import math
 import time
 
@@ -27,8 +33,8 @@ def getDigits(number):
     return digit_list
 
 def getProduct(start_range_m,end_range_m,start_range_n,end_range_n,pandigital_list):
-    for m in range(start_range_m,end_range_m):
-        for n in range(start_range_n,end_range_n):
+    for m in range(start_range_m,end_range_m + 1):
+        for n in range(start_range_n,end_range_n + 1):
             product = m * n
             number = int(str(m) + str(n) + str(product))
             if is_pandigital(number):
@@ -42,6 +48,7 @@ pandigital_list = getProduct(0,9,1000,9999,pandigital_list)
 pandigital_list = getProduct(10,99,100,999,pandigital_list)
 end_time = time.time()
 unique_pandigitals = set(pandigital_list)
+
 print "Took %s seconds to find sum of unique pandigitals: %s" % (end_time - start_time,sum(unique_pandigitals))
 print "The list of unique pandigitals is %s" % (unique_pandigitals)
 
