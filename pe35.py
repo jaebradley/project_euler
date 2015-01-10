7d59752a3212b77320b2b2f7e39d241a7e525aa7
jaebradley <jae.b.bradley@gmail.com>
2015-01-10 14:09:40 -0500

    project euler
    fixed #35 so that it works but is still too slow

--- pe35.py
+++ pe35.py
@@ -5,16 +5,12 @@ There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73
 
 How many circular primes are there below one million?
 
-1. Find all primes below 1 million
-2. For length of prime, rearrange prime by adding first value to end
-3. Evaluate if prime, if not, end
-4. If counter is equal to length of prime then the prime is a circular prime
-5. All primes can't contain a 2
 
-TOO SLOW
+TOO SLOW ~150 seconds, but can't think of anything that optimizes right now
 '''
 
 import numpy as numpy
+import time
 def sieve(n):
     """Return an array of the primes below n."""
     prime = numpy.ones(n//3 + (n%6==2), dtype=numpy.bool)
@@ -42,45 +38,56 @@ def getDigits(number):
         digit = number % 10
         digit_list = [digit] + digit_list
         number /= 10
-    return digit_list
+    return tuple(sorted(digit_list)),digit_list
 
-def checkCircularNumbers(digit_list):
-    is_circular_number = 1
-    all_numbers = []
-    if (5 in digit_list) or (0 in digit_list) or (2 in digit_list) or (4 in digit_list) or (6 in digit_list) or (8 in digit_list):
-        is_circular_number = 0
-    for i in range(0,len(digit_list) - 1):
-        digit = digit_list.pop(-1)
-        digit_list = [digit] + digit_list
-        number = int(''.join(str(dig) for dig in digit_list))
-        all_numbers.append(number)
-        if 1 == is_circular_number:
-            if (0 == number % 2) or (0 == number % 5) or (not is_prime(number,prime_list2)) :
-                is_circular_number = 0
-    return is_circular_number,tuple(all_numbers)
+start_time = time.time()
+primes = sieve(1000000)
+primes2 = sieve(9999999)
+circular_primes_dict = {}
+circular_primes_list = []
 
-def primeNotInCache(circular_numbers_tuple,is_circular,cached_map):
-    for number in circular_numbers_tuple:
-        if number not in cached_map:
-            cached_map[number] = is_circular
+for prime in primes:
+    if prime > 100:
+        print prime
+        digit_data = getDigits(prime)
+        key_number = prime
+        digit_list = digit_data[1]
+        new = 1
+        is_circular = 1
+        for digit in digit_list:
+            if (digit % 2 == 0) or (digit % 5 == 0):
+                circular_primes_dict[key_number] = {'value':0,'numbers':[prime]}
+                is_circular = 0
+                break
+        if is_circular == 1:
+            for key in circular_primes_dict:
+                if key_number in circular_primes_dict[key]['numbers']:
+                    new = 0
+                    break
+            if new == 1:
+                circular_primes_dict[key_number] = {'value':1,'numbers':[prime]}
+                for i in range(0,len(digit_list) - 1):
+                    digit = digit_list.pop(-1)
+                    digit_list = [digit] + digit_list
+                    number = int(''.join(str(dig) for dig in digit_list))
+                    circular_primes_dict[key_number]['numbers'].append(number)
+                    if is_prime(number,primes2) == 0:
+                        circular_primes_dict[key_number]['value'] = 0
+
+for key in circular_primes_dict:
+    if circular_primes_dict[key]['value'] == 1:
+        for number in circular_primes_dict[key]['numbers']:
+            circular_primes_list.append(number)
+
+unique_circular_primes = set(circular_primes_list)
+answer = len(unique_circular_primes) + 13
+end_time = time.time()
+seconds = end_time - start_time
+print "Took %s seconds to find answer: %s" % (seconds, answer)
 
 
-prime_list = sieve(1000000)
-cached_map = {}
-prime_list2 = sieve(2000000)
 
-for prime in prime_list:
-    if (prime >= 100) and (prime not in cached_map):
-        digit_list = getDigits(prime)
-        circular_number_results = checkCircularNumbers(digit_list)
-        is_circular = circular_number_results[0]
-        all_circular_numbers = circular_number_results[1]
-        primeNotInCache(all_circular_numbers,is_circular,cached_map)
-circular_list = []
-for key in cached_map:
-    if cached_map[key] == 1:
-        circular_list.append(key)
 
-print circular_list,len(circular_list)
+
 
 
