5d7bfc358c734ca5487f93f1d89b310a8f9d4822
jaebradley <jae.b.bradley@gmail.com>
2015-09-24 10:13:45 -0400

    utils/utility_methods.py

--- problems/pe5.py
+++ problems/pe5.py
@@ -7,15 +7,15 @@ What is the smallest positive number that is evenly divisible by all of the numb
 """
 Can calculate this without Python.
 
-If a number is divisible by 20, that also implies that is divisible by 10, 5, 4, 2, and 1.
-If a number is divisible by 18, that also implies that is divisible by 9, 6, 3, 2, and 1.
+If a number is divisible by 20, that also implies that it is divisible by 10, 5, 4, 2, and 1.
+If a number is divisible by 18, that also implies that it is divisible by 9, 6, 3, 2, and 1.
 
 Basically, following this logic, if a number is divisible by numbers 20-11 it is also
 divisible by numbers 10-1.
 
 So now we need to find the LCD of the numbers 20-11.  Since 19, 17, 13, and 11 are prime numbers we really need to find
-the LCD of 20, 18, 16, 15, 14, and 12 and multiply that LCD by the aforementioned prime numbers to find the
-LCD of numbers 20-11.
+the LCD of 20, 18, 16, 15, 14, and 12 and multiply it by the aforementioned prime numbers to find the LCD of numbers
+20-11.
 
 Using prime factorization, one can discern that the LCD of the composite numbers between 20-11 is 2^4 * 3^2 * 5 * 7,
 or 5040.  The product of 5040 and the primes between 20-11 is 232792560.
--- utils/utility_methods.py
+++ utils/utility_methods.py
@@ -1,11 +1,10 @@
 def is_palindrome(alphanumeric):
-    is_palindrome_boolean = True
     alphanumeric_string = str(alphanumeric)
     alphanumeric_string_length = alphanumeric_string.__len__()
     for start_position in range(0, alphanumeric_string_length):
         end_position = alphanumeric_string_length - 1 - start_position  # string is zero indexed but string length is one indexed
         if end_position < start_position:
-            return is_palindrome_boolean
+            return True
         else:
             if alphanumeric_string[start_position] != alphanumeric_string[end_position]:
                 return False
