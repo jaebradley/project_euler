0da306372a0832f77165561b4b9400fce48c6b04
jaebradley <jae.b.bradley@gmail.com>
2015-09-25 01:53:56 -0400

    completed problems 12 and 13, problem 11 is in active development

--- problems/pe13.py
+++ problems/pe13.py
@@ -1,7 +1,28 @@
-file = open('pe.txt') #open file with the one hundred 50-digit numbers
-output = file.read() #read from file
-output_list = output.split() #string output to list
-output_list = map(int,output_list) #list elements are string -- convert to integers
-sum = sum(output_list) #find the sum of all the list elements
-str_sum = str(sum) #convert to string to get the first 10 digits of sum
-print str_sum[0:10] #get first 10 digits of sum
+"""
+https://projecteuler.net/problem=13
+
+Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+"""
+
+import time
+
+
+def convert_text_file_to_list_of_integers(file_path):
+    output_list_of_strings = open(file_path).read().split()
+    output_list_of_integers = map(int, output_list_of_strings)  # convert string objects to integers
+    return output_list_of_integers
+
+
+def main(file_path):
+    start_time = time.time()
+
+    integer_list = convert_text_file_to_list_of_integers(file_path)
+    result_sum = sum(integer_list)
+    result_string = str(result_sum)[:10]
+
+    end_time = time.time()
+    execution_time_in_seconds = end_time - start_time
+
+    print "first ten digits are {0}; took {1} seconds".format(result_string, execution_time_in_seconds)
+
+main("../static/pe13.txt")
