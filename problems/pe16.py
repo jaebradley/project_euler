d0a52f8aeacd52bb06f0585031723a182c109374
jaebradley <jae.b.bradley@gmail.com>
2015-09-25 22:27:31 -0400

    problem 16

--- problems/pe16.py
+++ problems/pe16.py
@@ -1,7 +1,29 @@
-import math
-sum = 0
-num = '{0:f}'.format(math.pow(2,1000)).rstrip('0').rstrip('.') #get 2^1000, strip 0s after decimal point and then decimal point because python formatting sucks
+"""
+2 ** 15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
-for x in range(0,len(num)):
-    sum = sum + int(num[x]) #for each digit in number, sum the digits
-print sum
+What is the sum of the digits of the number 2 ** 1000?
+"""
+
+import time
+
+
+def sum_of_digits(number):
+    digit_sum = 0
+    stringified_number = str(number)
+    for index in range(0, stringified_number.__len__()):
+        digit_sum += int(stringified_number[index])
+    return digit_sum
+
+
+def main(number):
+    start_time = time.time()
+
+    digit_sum = sum_of_digits(number)
+
+    end_time = time.time()
+
+    execution_seconds = end_time - start_time
+
+    print "sum of digits for {0} is {1}; took {2} seconds".format(number, digit_sum, execution_seconds)
+
+main(2 ** 1000)
