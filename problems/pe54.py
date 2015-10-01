658293ecd817f54fd28b9209a36d84471abe7dcc
jaebradley <jae.b.bradley@gmail.com>
2015-09-30 22:50:11 -0400

    restructuring classes for problem 54

--- problems/pe54.py
+++ problems/pe54.py
@@ -81,241 +81,108 @@ class Hand:
 
     def __init__(self, cards):
         self.cards = cards
-        self.properties = None
-        self.hand_ranking = None
 
 
-class HandProperties:
+class HandRankingCalculator:
 
-    def __init__(self):
-        self.same_suit = None
-        self.pair_list = None
-        self.three_of_a_kind = None
-        self.four_of_a_kind = None
-        self.consecutive_card_values = None
+    def __init__(self, hand):
+        self.hand = hand
 
-
-class Pair:
-
-    def __init__(self, value):
-        self.value = value
-
-
-class ThreeOfAKindProperty:
-
-    def __init__(self, value):
-        self.value = value
-
-
-class FourOfAKindProperty:
-
-    def __init__(self, value):
-        self.value = value
-
-
-class ConsecutiveCardValues:
-
-    def __init__(self, highest_value):
-        self.highest_value = highest_value
-
-
-class SameSuit:
-
-    def __init__(self, suit):
-        self.suit = suit
-
-
-class HighCard:
-
-    def __init__(self, value, first_kicker, second_kicker, third_kicker, fourth_kicker):
-        self.value = value
-        self.first_kicker = first_kicker
-        self.second_kicker = second_kicker
-        self.third_kicker = third_kicker
-        self.fourth_kicker = fourth_kicker
-
-
-class OnePair:
-
-    def __init__(self, value, first_kicker, second_kicker, third_kicker, fourth_kicker):
-        self.value = value
-        self.first_kicker = first_kicker
-        self.second_kicker = second_kicker
-        self.third_kicker = third_kicker
-        self.fourth_kicker = fourth_kicker
-
-
-class TwoPair:
-
-    def __init__(self, high_pair_value, low_pair_value, kicker):
-        self.high_pair_value = high_pair_value
-        self.low_pair_value = low_pair_value
-        self.kicker = kicker
-
-
-class ThreeOfAKindHandRanking:
-
-    def __init__(self, value, first_kicker, second_kicker):
-        self.value = value
-        self.first_kicker = first_kicker
-        self.second_kicker = second_kicker
-
-
-class Straight:
-
-    def __init__(self, highest_value):
-        self.highest_value = highest_value
-
-
-class Flush:
-
-    def __init__(self, suit, first_kicker, second_kicker, third_kicker, fourth_kicker, fifth_kicker):
-        self.suit = suit
-        self.first_kicker = first_kicker
-        self.second_kicker = second_kicker
-        self.third_kicker = third_kicker
-        self.fourth_kicker = fourth_kicker
-        self.fifth_kicker = fifth_kicker
-
-
-class FullHouse:
-
-    def __init__(self, three_of_a_kind_value, pair_value):
-        self.three_of_a_kind_value = three_of_a_kind_value
-        self.pair_value = pair_value
-
-class FourOfAKindHandRanking:
-
-    def __init__(self, value, kicker):
-        self.value = value
-        self.kicker = kicker
-
-
-class StraightFlush:
-
-    def __init__(self, suit, highest_value):
-        self.suit = suit
-        self.highest_value = highest_value
-
-
-def is_same_suit(hand):
-    if [card.suit for card in hand.card][1:] == [card.suit for card in hand.card][:-1]:
-        return SameSuit(suit=hand.cards[0].suit)
-    else:
-        return False
-
-
-def calculate_non_distinct_card_value_sets(cards, distinct_card_values, hand_properties):
-    card_counter = Counter(cards)
-    for distinct_card_value in distinct_card_values:
-        card_value_count = card_counter[distinct_card_value]
-        if card_value_count == 2:
-            hand_properties.pair_list.append(Pair(distinct_card_value))
-        elif card_value_count == 3:
-            hand_properties.three_of_a_kind = (ThreeOfAKindProperty(distinct_card_value))
-        elif card_value_count == 4:
-            hand_properties.four_of_a_kind = (FourOfAKindProperty(distinct_card_value))
+    def is_flush(self):
+        if [card.suit for card in self.hand.card][1:] == [card.suit for card in self.hand.card][:-1]:
+            return True
         else:
-            RuntimeError("unexpected card value count")
+            return False
 
-
-def calculate_hand_properties(hand):
-
-    hand_properties = HandProperties()
-
-    # check if any cards of the same value
-    distinct_card_values = sorted(set([card.high_value for card in hand.cards]), reverse=True)
-    if distinct_card_values.__len__() < 5:
-        calculate_non_distinct_card_value_sets(hand.cards, distinct_card_values, hand_properties)
-    # if distinct card values equals starting card values then no duplicate cards
-    else:
-        same_suit = is_same_suit(hand)
-        if same_suit != False:
-            hand_properties.same_suit = same_suit
-
-        sorted_cards_by_high_value = sorted([card.high_value for card in hand.cards], reverse=True)
+    def is_high_value_straight(self):
+        sorted_cards_by_high_value = sorted([card.high_value for card in self.hand.cards], reverse=True)
         is_consecutive_card_high_values = True
         for card_index in range(0, 4):
             if sorted_cards_by_high_value[card_index] - 1 != sorted_cards_by_high_value[card_index + 1]:
                 is_consecutive_card_high_values = False
         if is_consecutive_card_high_values:
-            hand_properties.consecutive_card_values = (ConsecutiveCardValues(highest_value=sorted_cards_by_high_value[0]))
+            return True
         else:
-            sorted_cards_by_low_value = sorted([card.low_value for card in hand.cards], reverse=True)
-            is_consecutive_card_low_values = True
-            for card_index in range(0, 4):
-                if sorted_cards_by_low_value[card_index] - 1 != sorted_cards_by_low_value[card_index + 1]:
-                    is_consecutive_card_low_values = False
-            if is_consecutive_card_low_values:
-                hand_properties.consecutive_card_values = (ConsecutiveCardValues(
-                    highest_value=sorted_cards_by_low_value[0]))
+            return False
 
-    hand.properties = hand_properties
+    def is_low_value_straight(self):
+        sorted_cards_by_low_value = sorted([card.low_value for card in self.hand.cards], reverse=True)
+        is_consecutive_card_low_values = True
+        for card_index in range(0, 4):
+            if sorted_cards_by_low_value[card_index] - 1 != sorted_cards_by_low_value[card_index + 1]:
+                is_consecutive_card_low_values = False
+        if is_consecutive_card_low_values:
+            return True
+        else:
+            return False
 
+    def is_straight(self):
+        if self.is_high_value_straight() or self.is_low_value_straight():
+            return True
+        else:
+            return False
 
-def calculate_hand_ranking(hand):
-    if hand.properties.consecutive_card_values is not None and hand.properties.same_suit is not None:
-        hand_ranking = StraightFlush(hand.properties.same_suit, hand.properties.consecutive_card_values.highest_value)
-    elif hand.properties.four_of_a_kind is not None:
-        kicker = [card.high_value for card in hand.cards if card.high_value != hand.properties.four_of_a_kind.value][0]
-        hand_ranking = FourOfAKindHandRanking(hand.properties.four_of_a_kind.value, kicker)
-    elif hand.properties.three_of_a_kind is not None and hand.properties.pair_list.__len__() == 1:
-        hand_ranking = FullHouse(hand.properties.three_of_a_kind.value, hand.properties.pair_list[0].value)
-    elif hand.properties.same_suit is not None:
-        sorted_cards_by_high_value = sorted([card.high_value for card in hand.cards], reverse=True)
-        hand_ranking = Flush(
-            hand.properties.same_suit,
-            sorted_cards_by_high_value[0],
-            sorted_cards_by_high_value[1],
-            sorted_cards_by_high_value[2],
-            sorted_cards_by_high_value[3],
-            sorted_cards_by_high_value[4]
-        )
-    elif hand.properties.consecutive_card_values is not None:
-        sorted_cards_by_high_value = sorted([card.high_value for card in hand.cards])[0]
-        highest_value = sorted_cards_by_high_value[0]
-        hand_ranking = Straight(highest_value)
-    elif hand.properties.three_of_a_kind is not None:
-        kickers = sorted([card.high_value for card in hand.cards if card.high_value != hand.properties.three_of_a_kind.value], reverse=True)
-        hand_ranking = ThreeOfAKindHandRanking(
-            hand.properties.three_of_a_kind.value,
-            kickers[0],
-            kickers[1]
-        )
-    elif hand.properties.pair_list.__len__() == 2:
-        kicker = [card.high_value for card in hand.cards if card.high_value != hand.properties.pair_list[0].value or card.high_value != hand.properties.pair_list[1].value][0]
-        hand_ranking = TwoPair(
-            hand.properties.pair_list[0].value,
-            hand.properties.pair_list[1].value,
-            kicker
-        )
-    elif hand.properties.pair_list.__len__() == 1:
-        kickers = sorted([card.high_value for card in hand.cards if card.high_value != hand.properties.pair_list[0].value], reverse=True)
-        hand_ranking = OnePair(
-            hand.properties.pair_list[0].value,
-            kickers[0],
-            kickers[1],
-            kickers[2],
-            kickers[3]
-        )
-    else:
-        sorted_cards_by_high_value = sorted([card.high_value for card in hand.cards], reverse=True)
-        hand_ranking = HighCard(
-            sorted_cards_by_high_value[0],
-            sorted_cards_by_high_value[1],
-            sorted_cards_by_high_value[2],
-            sorted_cards_by_high_value[3],
-            sorted_cards_by_high_value[4]
-        )
-    hand.hand_ranking = hand_ranking
+    def is_straight_flush(self):
 
+        if self.is_flush() and self.is_straight():
+            return True
+        else:
+            return False
 
-def calculate_winners(hands):
-    straight_flushes = [hand for hand in hands if isinstance(hand.hand_ranking, StraightFlush)]
-    if straight_flushes.__len__() > 0:
-        
+    def is_four_of_a_kind(self):
+        distinct_card_values = list(set([card.high_value for card in self.hand.cards]))
+        card_counter = Counter(self.hand.cards)
+        if distinct_card_values.__len__() == 2 and (card_counter[distinct_card_values[0]] == 4 or card_counter[distinct_card_values[0]] == 1):
+            return True
+        else:
+            return False
+
+    def is_full_house(self):
+        distinct_card_values = list(set([card.high_value for card in self.hand.cards]))
+        card_counter = Counter(self.hand.cards)
+        if distinct_card_values.__len__() == 2 and (card_counter[distinct_card_values[0]] == 3 or card_counter[distinct_card_values[0]] == 2):
+            return True
+        else:
+            return False
+
+    def is_three_of_a_kind(self):
+        distinct_card_values = list(set([card.high_value for card in self.hand.cards]))
+        card_counter = Counter(self.hand.cards)
+        if distinct_card_values.__len__() == 3 and (card_counter[distinct_card_values[0]] == 3 or card_counter[distinct_card_values[1]] == 3 or card_counter[distinct_card_values[2]] == 3):
+            return True
+        else:
+            return False
+
+    def is_two_pair(self):
+        distinct_card_values = list(set([card.high_value for card in self.hand.cards]))
+        card_counter = Counter(self.hand.cards)
+        if distinct_card_values.__len__() == 3 and (card_counter[distinct_card_values[0]] == 2 or card_counter[distinct_card_values[1]] == 2 or card_counter[distinct_card_values[2]] == 2):
+            return True
+        else:
+            return False
+
+    def is_one_pair(self):
+        distinct_card_values = list(set([card.high_value for card in self.hand.cards]))
+        if distinct_card_values.__len__() == 4:
+            return True
+        else:
+            return False
+
+    def is_high_card(self):
+        distinct_card_values = list(set([card.high_value for card in self.hand.cards]))
+        if not self.is_straight() and not self.is_flush() and distinct_card_values.__len__() == 5:
+            return True
+        else:
+            return False
 
 
+class WinningHandSelector:
+
+    def __init__(self, handA, handB):
+        self.handA = handA
+        self.handB = handB
+
+    
 
 
 
