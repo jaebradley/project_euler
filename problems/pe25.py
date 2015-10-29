bffd4b9935cbb81b55ed44e8eab44279fd499a8b
jaebradley <jae.b.bradley@gmail.com>
2015-10-28 23:16:05 -0400

    updating problem 25

--- problems/pe25.py
+++ problems/pe25.py
@@ -1,22 +1,58 @@
-__author__ = 'jaebradley'
+# coding=utf-8
+"""
+The Fibonacci sequence is defined by the recurrence relation:
 
-#find the first fibonacci number with 1000 digits
-import math;
+Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+Hence the first 12 terms will be:
 
-def dig_count(num):
-    digits = int(math.log10(num))+1; #calculate the digits of a number
-    return digits;
+F1 = 1
+F2 = 1
+F3 = 2
+F4 = 3
+F5 = 5
+F6 = 8
+F7 = 13
+F8 = 21
+F9 = 34
+F10 = 55
+F11 = 89
+F12 = 144
+The 12th term, F12, is the first term to contain three digits.
 
-def fib(dig):
-    data=[1,1]; #store initial fibonacci numbers
-    ans=2; #set some arbitrary non-zero value for ans
-    i = 1
-    while dig_count(ans) < dig: #while digit count is greater than the digit count specified calculate next fibonacci number and then store
-        n=len(data);
-        ans=data[n-1] + data[n-2];
-        data.append(ans); #store fibonacci number
-        i = i + 1
-        print i+1
-    return data[n];
+What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+"""
 
-print fib(1000)
+import math
+import time
+
+
+def return_number_digit_count(number):
+    digit_count = int(math.log10(number))+1
+    return digit_count
+
+
+def return_index_of_the_first_fibonacci_number_with_digit_count_limit(fibonacci_digit_limit):
+    # store initial fibonacci numbers
+    data = [1, 1]
+    answer = 1
+    # while digit count is greater than the digit count specified calculate next fibonacci number and then store
+    index_value = 2
+    while return_number_digit_count(answer) < fibonacci_digit_limit:
+        n = len(data)
+        answer = data[n-1] + data[n-2]
+        data.append(answer)
+        index_value += 1
+    return index_value
+
+
+def main(fibonacci_digit_limit):
+    start_time = time.time()
+
+    fibonacci_number_index_value = return_index_of_the_first_fibonacci_number_with_digit_count_limit(fibonacci_digit_limit)
+
+    end_time = time.time()
+    execution_seconds = end_time - start_time
+    print "index value for first Fibonacci number to contain {0} digits is {1}; execution took {2} seconds".format(fibonacci_digit_limit, fibonacci_number_index_value, execution_seconds)
+
+
+main(1000)
