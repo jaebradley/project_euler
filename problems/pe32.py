cbdf78aad6e12bd1d08b3aafa5e1f043c02df7f7
jaebradley <jae.b.bradley@gmail.com>
2015-11-02 17:36:47 -0500

    problem 32

--- problems/pe32.py
+++ problems/pe32.py
@@ -1,30 +1,34 @@
-__author__ = 'jaebradley'
-
-'''
+"""
 Want to find all numbers for m * n = z where concat(m, n, z) uses all digits from 1-9 exactly once
 Only 2 ways to get this: either m is a 1 digit number and n is a 4 digit number OR m is a 2 digit number and n is a 3 digit number
 So iterate through all possible m and n and check if concat(m, n, z) is pandigital
-'''
+Return the sum of all valid pandigital identities
+"""
 
-import math
 import time
 
-def is_pandigital(number):
-    digit_list = getDigits(number)
-    if (max(digit_list) != len(digit_list)) or (min(digit_list) != 1):
-        return 0
-    else:
-        for i in range(1,len(digit_list) + 1):
-            try:
-                digit_list.remove(i)
-            except ValueError:
-                return 0
-        if not digit_list:
-            return 1
-        else:
-            return 0
 
-def getDigits(number):
+def is_pandigital(number):
+    digit_list = return_number_digit_list(number)
+
+    if max(digit_list) != 9:
+        return False
+
+    if min(digit_list) != 1:
+        return False
+
+    sorted_digit_list = sorted(digit_list)
+    index_counter = 0
+    for sorted_digit in sorted_digit_list:
+        if sorted_digit != index_counter + 1:
+            return False
+        else:
+            index_counter += 1
+
+    return True
+
+
+def return_number_digit_list(number):
     digit_list = []
     while number:
         digit = number % 10
@@ -32,25 +36,35 @@ def getDigits(number):
         number /= 10
     return digit_list
 
-def getProduct(start_range_m,end_range_m,start_range_n,end_range_n,pandigital_list):
+
+def return_pandigital_list_for_range(start_range_m, end_range_m, start_range_n, end_range_n):
+    pandigital_candidate_list = list()
     for m in range(start_range_m,end_range_m + 1):
         for n in range(start_range_n,end_range_n + 1):
             product = m * n
             number = int(str(m) + str(n) + str(product))
             if is_pandigital(number):
-                pandigital_list.append(product)
-    return pandigital_list
+                pandigital_candidate_list.append(product)
+    return pandigital_candidate_list
 
-start_time = time.time()
-pandigital_list = []
 
-pandigital_list = getProduct(0,9,1000,9999,pandigital_list)
-pandigital_list = getProduct(10,99,100,999,pandigital_list)
-end_time = time.time()
-unique_pandigitals = set(pandigital_list)
+def return_pandigital_sum():
+    pandigital_candidate_list = list()
+    pandigital_candidate_list.append(return_pandigital_list_for_range(0, 9, 1000, 9999))
+    pandigital_candidate_list.append(return_pandigital_list_for_range(10, 99, 100, 999))
+    return set(pandigital_candidate_list)
 
-print "Took %s seconds to find sum of unique pandigitals: %s" % (end_time - start_time,sum(unique_pandigitals))
-print "The list of unique pandigitals is %s" % (unique_pandigitals)
+
+def main():
+    start_time = time.time()
+
+    pandigital_sum = return_pandigital_sum()
+
+    end_time = time.time()
+    execution_seconds = end_time - start_time
+    print "sum of all pandigital multiplicand/multiplier/product identities is {0}; execution took {1} seconds".format(pandigital_sum, execution_seconds)
+
+main()
 
 
 
