0d114682f1a9e64e791f3665ee24da1250183c2e
jaebradley <jae.b.bradley@gmail.com>
2015-11-03 11:36:37 -0500

    project euler problem 33 solved

--- problems/pe33.py
+++ problems/pe33.py
@@ -8,30 +8,7 @@ There are exactly four non-trivial examples of this type of fraction, less than 
 If the product of these four fractions is given in its lowest common terms, find the value of the denominator.
 """
 from __future__ import division
-
-
-# num_log = []
-#
-# for z in range(11, 100):
-#     for i in range(10, 100):
-#             str_i = str(i)
-#             str_z = str(z)
-#             x0 = float(str_i[0])
-#             x1 = float(str_i[1])
-#             x2 = float(str_z[0])
-#             x3 = float(str_z[1])
-#             num = float(i)/float(z)
-#             if x0 != 0.0 and x1 != 0.0 and x2 != 0.0 and x3 != 0.0:
-#                 if x0 == x2 and x1/x3 < 1 and x1/x3 == num:
-#                     num_log.append([i,z])
-#                 elif x0 == x3 and x1/x2 < 1 and x1/x2 == num:
-#                     num_log.append([i,z])
-#                 elif x1 == x2 and x0/x3 < 1 and x0/x3 == num:
-#                     num_log.append([i,z])
-#                 elif x1 == x3 and x0/x2 < 1 and x0/x2 == num:
-#                     num_log.append([i,z])
-#
-# print num_log
+import time
 
 
 def is_naive_non_trivial_simplification(numerator, denominator):
@@ -47,33 +24,44 @@ def is_naive_non_trivial_simplification(numerator, denominator):
     if (0 == ones_digit_numerator) or (0 == ones_digit_denominator):
         return False
 
-    # four possible combinations
-    decimal_value = numerator / denominator
+    if tens_digit_numerator == tens_digit_denominator and 1 != ones_digit_numerator / ones_digit_denominator:
+        candidate_value = ones_digit_numerator / ones_digit_denominator
 
-    # four possible candidate values
-    candidate_value_1 = tens_digit_numerator / tens_digit_denominator
-    candidate_value_2 = tens_digit_numerator / ones_digit_denominator
-    candidate_value_3 = ones_digit_numerator / tens_digit_denominator
-    candidate_value_4 = ones_digit_numerator / ones_digit_denominator
+    elif tens_digit_numerator == ones_digit_denominator and 1 != ones_digit_numerator / tens_digit_denominator:
+        candidate_value = ones_digit_numerator / tens_digit_denominator
 
-    # test candidate values
-    if candidate_value_1 == decimal_value:
-        return True
-    elif candidate_value_2 == decimal_value:
-        return True
-    elif candidate_value_3 == decimal_value:
-        return True
-    elif candidate_value_4 == decimal_value:
+    elif ones_digit_numerator == tens_digit_denominator and 1 != tens_digit_numerator / ones_digit_denominator:
+        candidate_value = tens_digit_numerator / ones_digit_denominator
+
+    elif ones_digit_numerator == ones_digit_denominator and 1 != tens_digit_denominator / tens_digit_numerator:
+        candidate_value = tens_digit_denominator / tens_digit_numerator
+
+    else:
+        return False
+
+    if candidate_value == numerator / denominator:
         return True
     else:
         return False
 
 
-def return_naive_non_trivial_simplification_denominators():
+def return_sum_of_naive_non_trivial_simplification_denominators():
     denominators = list()
     for candidate_numerator in range(10, 100):
-        for candidate_denominator in range(10, 100):
+        for candidate_denominator in range(candidate_numerator + 1, 100):
             if is_naive_non_trivial_simplification(numerator=candidate_numerator, denominator=candidate_denominator):
                 denominators.append(candidate_denominator)
-    return denominators
+    return sum(denominators)
 
+
+def main():
+    start_time = time.time()
+
+    denominator_sum = return_sum_of_naive_non_trivial_simplification_denominators()
+
+    end_time = time.time()
+    execution_seconds = end_time - start_time
+    print "sum of denominators is {0}; execution took {1} seconds".format(denominator_sum, execution_seconds)
+
+
+main()
