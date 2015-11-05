6e447ddbe17918bd60fe0125909f21225b872b18
jaebradley <jae.b.bradley@gmail.com>
2015-11-05 08:50:48 -0500

    problem 31 coin logic restructuring

--- problems/pe31.py
+++ problems/pe31.py
@@ -51,26 +51,56 @@ class TwoPound(Coin):
         Coin.__init__(self, 200)
 
 
-value_dict = {key: 0 for key in range(0, 201)}
+class CoinTypes:
+    def __init__(
+            self,
+            include_one_pence,
+            include_two_pence,
+            include_five_pence,
+            include_ten_pence,
+            include_twenty_pence,
+            include_fifty_pence,
+            include_one_pound,
+            include_two_pound):
+        self.coins = list()
+        if include_one_pence:
+            self.coins.append(OnePence())
 
-one = 1
-two = 2
-five = 5
-ten = 10
-twenty = 20
-fifty = 50
-pound = 100
-two_pound = 200
+        if include_two_pence:
+            self.coins.append(TwoPence())
 
-value_dict[0] = 1
+        if include_five_pence:
+            self.coins.append(FivePence())
 
-coin_list = [one, two, five, ten, twenty, fifty, pound, two_pound]
-for coin_value in coin_list:
-    for key in value_dict:
-        if key >= coin_value:
-            lookup_key = key - coin_value
-            value_dict[key] += value_dict[lookup_key]
+        if include_ten_pence:
+            self.coins.append(TenPence())
 
-print value_dict[200]
+        if include_twenty_pence:
+            self.coins.append(TwentyPence())
+
+        if include_fifty_pence:
+            self.coins.append(FiftyPence())
+
+        if include_one_pound:
+            self.coins.append(OnePound())
+
+        if include_two_pound:
+            self.coins.append(TwoPound())
+
+
+def calculate_different_combinations_of_coins_to_make_value(coin_types, value):
+    value_dict = {key: 0 for key in range(0, value + 1)}
+    value_dict[0] = 1
+
+    for coin in coin_types.coins:
+        for key in value_dict:
+            if key >= coin.value:
+                lookup_key = key - coin.value
+                value_dict[key] += value_dict[lookup_key]
+
+    return value_dict[value]
+
+coin_types = CoinTypes(True, True, True, True, True, True, True, True)
+print calculate_different_combinations_of_coins_to_make_value(coin_types=coin_types, value=200)
 
 
