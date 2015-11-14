75df416e27b4898bbd4b902362dcf16acf4ff98b
Jae Bradley <jae.b.bradley@gmail.com>
2015-11-14 10:04:35 -0500

    problem 36

--- problems/pe36.py
+++ problems/pe36.py
@@ -1,44 +1,38 @@
-__author__ = 'jaebradley'
-'''
-base_two_list = []
-word = ""
-for i in range(1, 1000001):
-    str_i = str(i)
-    for letter in str_i:
-        word += letter
+"""
+https://projecteuler.net/problem=36
+Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
 
-    while i  > 0:
-        number, multiplier = 0, 1
-        number += i % 2 * multiplier
-        multiplier *= 10
-        number = number // 2
-'''
+(Please note that the palindromic number, in either base, may not include leading zeros.)
+"""
+from utils.utilities import is_palindrome
+import time
 
-def is_palindrome(i):
-    str_i = str(i)
-    word = ""
-    if len(str_i) >= 1:
-        for z in range(len(str_i) - 1, -1, -1):
-            for letter in str_i[z]:
-                word += letter
-        if int(word) == i:
-            return 1
 
-def base_two(n):
+def return_base_two_representation(base_10_number):
     number, multiplier = 0, 1
-    while n > 0:
-        number += n % 2 * multiplier
+    while base_10_number > 0:
+        number += base_10_number % 2 * multiplier
         multiplier *= 10
-        n = n/2
+        base_10_number /= 2
     return number
 
 
-def double_palindrome():
-    num_list = []
-    for i in range(1,1000001):
-        if (is_palindrome(base_two(i)) == 1) and (is_palindrome(i) == 1):
-            num_list.append(i)
-            print i, base_two(i)
-    return sum(num_list)
+def return_base_2_and_base_10_palindrome_sum(limit):
+    double_palindrome_sum = 0
+    for candidate in range(1, limit + 1):
+        if is_palindrome(return_base_two_representation(base_10_number=candidate)) and is_palindrome(candidate):
+            double_palindrome_sum += candidate
+    return double_palindrome_sum
 
-double_palindrome_sum = double_palindrome()
+
+def main(limit):
+    start_time = time.time()
+
+    double_palindrome_sum = return_base_2_and_base_10_palindrome_sum(limit=limit)
+
+    end_time = time.time()
+    execution_seconds = end_time - start_time
+    print "sum of double palindromes is {0}; took {1} seconds".format(double_palindrome_sum, execution_seconds)
+
+
+main(limit=1000000)
