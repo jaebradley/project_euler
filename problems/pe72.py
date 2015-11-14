135d0ff6d41a50baa43fe173463ca3a73341cfb6
Jae Bradley <jae.b.bradley@gmail.com>
2015-11-14 12:19:39 -0500

    problem 72 refactor

--- problems/pe72.py
+++ problems/pe72.py
@@ -16,16 +16,42 @@ Took solution from: http://www.mathblog.dk/project-euler-72-reduced-proper-fract
 Euler Totient Function: https://en.wikipedia.org/wiki/Euler%27s_totient_function
 """
 from __future__ import division
+import time
 
-limit = 1000000
-phi = [x for x in range(0, limit + 1)]
-result = 0
 
-for i in range(2, limit + 1):
-    if i == phi[i]:
-        for j in range(i, limit + 1, i):
-            phi[j] = (phi[j] / i) * (i - 1)
+def return_reduced_proper_fraction_count_for_d(d_upper_limit_inclusive):
+    """
+    Basically, for each number from 2 to upper limit, divide any multiples by it,
+    ensuring that any non-zero remaining numbers are count of all relatively prime divisors
+    :param d_upper_limit_inclusive:
+    :return:
+    """
+    phi = [x for x in range(0, d_upper_limit_inclusive + 1)]
+    result = 0
 
-    result += phi[i]
+    for i in range(2, d_upper_limit_inclusive + 1):
+        if i == phi[i]:
+            for j in range(i, d_upper_limit_inclusive + 1, i):
+                phi[j] = (phi[j] / i) * (i - 1)
 
-print result
+        result += phi[i]
+
+    return result
+
+
+def main(d_upper_limit_inclusive):
+    start_time = time.time()
+
+    reduced_proper_fraction_count = return_reduced_proper_fraction_count_for_d(
+        d_upper_limit_inclusive=d_upper_limit_inclusive
+    )
+
+    end_time = time.time()
+    execution_seconds = end_time - start_time
+    print "number of reduced proper fractions for d <= {0} is {1}; took {2} seconds".format(
+        d_upper_limit_inclusive,
+        reduced_proper_fraction_count,
+        execution_seconds
+    )
+
+main(1000000)
