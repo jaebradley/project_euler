50effb50c7c8f90f5272d66ed4669670223e688d
Jae Bradley <jae.b.bradley@gmail.com>
2015-11-14 01:48:33 -0500

    refactoring problem 40

--- problems/pe40.py
+++ problems/pe40.py
@@ -1,6 +1,6 @@
-__author__ = 'jaebradley'
+"""
+https://projecteuler.net/problem=40
 
-'''
 Problem:
 
 An irrational decimal fraction is created by concatenating the positive integers:
@@ -20,31 +20,44 @@ Strategy:
     adding the next integer might make the string go from a length of 999,998 to a length of 1,000,002.
 3. If this is the case, I simply get the first one million characters of the string.
 4. For any given character position in the string, return the integer equivalent so that we can use them to multiply their values.
-'''
+"""
+import time
 
-def stringBuilder():
 
-    append_number = 1
-    number_string = '1'
-    max_string_length = 1000000
+def return_fractional_string(length):
+    fractional_string = str()
+    for integer in range(1, length + 2):
+        for digit in str(integer):
+            if len(fractional_string) < length:
+                fractional_string += digit
+    return fractional_string
 
-    while len(number_string) < max_string_length:
-        append_number += 1
-        append_number_string = str(append_number)
-        number_string += append_number_string
-        if len(number_string) > max_string_length:
-            return number_string[:max_string_length]
 
-def nthIntGetter (n,string):
-    return int(string[n - 1])
+def return_product_of_fractional_digit_indices(fractional_string_length, *indices):
+    fractional_string = return_fractional_string(length=fractional_string_length)
+    fractional_digit_product = 1
+    for index in indices:
+       fractional_digit_product *= int(fractional_string[index - 1])
+    return fractional_digit_product
 
-string = stringBuilder()
-d1 = nthIntGetter(1, string)
-d10 = nthIntGetter(10, string)
-d100 = nthIntGetter(100, string)
-d1000 = nthIntGetter(1000, string)
-d10000 = nthIntGetter(10000, string)
-d100000 = nthIntGetter(100000, string)
-d1000000 = nthIntGetter(1000000, string)
-value = d1 * d10 * d100 * d1000 * d10000 * d100000 * d1000000
-print value, d1, d10, d100, d1000, d10000, d100000, d1000000
+
+def main(fractional_string_length):
+    start_time = time.time()
+
+    product = return_product_of_fractional_digit_indices(
+        fractional_string_length,
+        1,
+        10,
+        100,
+        1000,
+        10000,
+        100000,
+        1000000,
+    )
+
+    end_time = time.time()
+    execution_seconds = end_time - start_time
+    print "product is {0}; took {1} seconds".format(product, execution_seconds)
+
+
+main(1000000)
