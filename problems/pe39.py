218eec0690949df743087550d9fef8e3afbccd49
Jae Bradley <jae.b.bradley@gmail.com>
2015-11-14 02:37:35 -0500

    problem 39

--- problems/pe39.py
+++ problems/pe39.py
@@ -1,46 +1,45 @@
 """
+https://projecteuler.net/problem=39
+
 If p is the perimeter of a right triangle, which value for p <= 1000 has the most solutions?
 """
-import math
+import time
 
-count_dict = {}
-for x in range(0,1001):
-    count_dict[x] = 0
 
-print count_dict[12.0]
-count = 0
-p = 1000
-for a in range(1,p - 1):
-    count = 0
-    print a
-    for b in range(1,p - 1):
-        c = (a ** 2 + b ** 2) ** 0.5
-        if a + b + c <= 1000 and c.is_integer() == 1:
-            sum = int(a + b + c)
-            count_dict[sum] += 1
+def return_pythagorean_triplets(perimeter):
+    triplets = list()
+    for x in range(1, perimeter - 2):
+        for y in range(x + 1, perimeter - x - 1):
+            z = perimeter - x - y
+            if z ** 2 == x ** 2 + y ** 2:
+                triplets.append([x, y, z])
+    return triplets
 
-""" a) create a list of the dict's keys and values;
-         b) return the key with the max value"""
 
-def keywithmaxval(d):
-     v=list(d.values())
-     k=list(d.keys())
-     return k[v.index(max(v))]
+def return_perimeter_with_most_pythagorean_triplet_solutions(upper_bound_perimeter):
+    solution_dict = {
+        'solution_count': 0,
+        'value': 0
+    }
+    for candidate_perimeter in range(upper_bound_perimeter, 0, -1):
+        print candidate_perimeter
+        pythagorean_solutions = return_pythagorean_triplets(perimeter=candidate_perimeter)
+        if len(pythagorean_solutions) > solution_dict['solution_count']:
+            solution_dict['solution_count'] = len(pythagorean_solutions)
+            solution_dict['value'] = candidate_perimeter
+    return solution_dict['value']
 
-key = keywithmaxval(count_dict)
-print key,count_dict[key]
 
-def return_pythagorean_triplets_which_sum_under_limit(limit):
-    pythagorean_triplets = list()
-    side_length_limit = int(math.floor(limit / 2))
-    for a in range(1, side_length_limit):
-        for b in range(a + 1, side_length_limit):
-            hypotenuse_squared = a ** 2 + b ** 2
-            # TODO: come up with a better integer identifier
-            if int(hypotenuse_squared) == hypotenuse_squared:
-                hypotenuse = math.sqrt(hypotenuse_squared)
-                if int(hypotenuse) == hypotenuse and a + b + hypotenuse <= limit:
-                    pythagorean_triplets.append(a + b + hypotenuse)
-    return pythagorean_triplets
+def main(upper_bound_perimeter):
+    start_time = time.time()
 
-print return_pythagorean_triplets_which_sum_under_limit(1000)
+    solution_perimeter = return_perimeter_with_most_pythagorean_triplet_solutions(
+        upper_bound_perimeter=upper_bound_perimeter
+    )
+
+    end_time = time.time()
+    execution_seconds = end_time - start_time
+    print "perimeter value with the most solutions is {0}; took {1} seconds".format(solution_perimeter, execution_seconds)
+
+
+main(1000)
